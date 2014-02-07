cf9797e48dce4c6ef46fcfde6e46c18ec7e0247a
Sanchit Bareja <sanchitbareja@gmail.com>
2014-02-07 14:44:37 -0800

    added media urls to urls.py

--- evalty/urls.py
+++ evalty/urls.py
@@ -8,4 +8,4 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
