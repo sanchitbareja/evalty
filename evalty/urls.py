06cc016cee1278855a93ad75033548361592273d
Sanchit Bareja <sanchitbareja@gmail.com>
2014-02-09 14:26:24 -0800

    changing urls

--- evalty/urls.py
+++ evalty/urls.py
@@ -3,13 +3,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
+from classes.views import home, student, professor
+
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'classes.views.home', name='home'),
-    url(r'^student/$', 'classes.views.student', name='student'),
-    url(r'^professor/$', 'classes.views.professor', name='prof'),
+    url(r'^$', home, name='home'),
+    url(r'^student/$', student, name='student'),
+    url(r'^professor/$', professor, name='prof'),
 
     url(r'^admin/', include(admin.site.urls)),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
