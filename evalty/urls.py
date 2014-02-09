100906c1c00cd01196245591fe87c1f32194648c
Sanchit Bareja <sanchitbareja@gmail.com>
2014-02-09 14:49:14 -0800

    hopefully this works for serving static files

--- evalty/urls.py
+++ evalty/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from classes.views import home, student, professor
 
@@ -14,3 +15,9 @@ urlpatterns = patterns('',
     url(r'^professor/$', professor, name='prof'),
     url(r'^admin/', include(admin.site.urls)),
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += patterns('django.contrib.staticfiles.views',
+    url(r'^static/(?P<path>.*)$', 'serve'),
+)
+
+urlpatterns += staticfiles_urlpatterns()
